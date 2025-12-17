0ed96f7e3ce3b65ca3d3a6f57bcf7f4bdaf6411d
Your Name <anthony197542@gmail.com>
2025-12-17 15:16:41 -0500

    2com

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -27,17 +27,21 @@ const Book = ({ book }) => {
        }
       </div>
       <div className="book__price">
-        
-          book.salePrice ? (
-        <> <span className="book__price--normal">${book.originalPrice.toFixed(2)}</span>$
-        ${book.salesPrice.toFixed(2)}
-        </>
+        <div className="book__price">
+  {
+  book.salePrice ? (
+    <> <span className="book__price--normal">${book.originalPrice.toFixed(2)}</span>
+      ${book.salePrice.toFixed(2)}
+    </>
   ) : (
-           <>${ book.originalPrice.toFixed(2) }</>
-       )
-     
-      </div>
-    </div>
+    <>${book.originalPrice.toFixed(2) }</>
+  }
+</div>
+</div>
+
+
   );
 }
 export default Book;
+
+    
