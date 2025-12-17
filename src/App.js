4da0e6175bf29a94b60ad33ab004c8f0733d921c
Your Name <anthony197542@gmail.com>
2025-12-17 13:56:29 -0500

    2com

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Nav from './components/Nav';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from "./pages/Home";
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,76 +1,43 @@
-import React, { useState, useEffect } from "react";
-import Book from "../components/Book";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
-const Books = ({ books: initalBooks }) => {
-  const [books, setBooks] = useState(initalBooks);
-
-  useEffect(() => {
-    setBooks(initalBooks);
-  }, [initalBooks]);
-
-  function filterBooks(filter) {
-    switch (filter) {
-      case "LOW_TO_HIGH":
-        return setBooks(
-          books
-            .slice()
-            .sort(
-              (a, b) =>
-                (a.salePrice || a.originalPrice) -
-                (b.salePrice || b.originalPrice)
-            )
-        );
-      case "HIGH_TO_LOW":
-        return setBooks(
-          books
-            .slice()
-            .sort(
-              (a, b) =>
-                (b.salePrice || b.originalPrice) -
-                (a.salePrice || a.originalPrice)
-            )
-        );
-      case "RATING":
-        return setBooks(books.slice().sort((a, b) => b.rating - a.rating));
-      default:
-        break;
-    }
-  }
-
+const Book = ({ book }) => {
   return (
-    <div id="books__body">
-      <main id="books__main">
-        <section>
-          <div className="books__container">
-            <div className="row">
-              <div className="books__header">
-                <h2 className="section__title books__header--title">
-                  All Books
-                </h2>
-                <select
-                  id="filter"
-                  onChange={(event) => filterBooks(event.target.value)}
-                  defaultValue={"DEFAULT"}
-                >
-                  <option value="DEFAULT" disabled>
-                    Sort
-                  </option>
-                  <option value="LOW_TO_HIGH">Price, Low to High</option>
-                  <option value="HIGH_TO_LOW">Price, High to Low</option>
-                  <option value="RATING">Rating</option>
-                </select>
-              </div>
-              <div className="books">
-                {books && books.map((book) => {
-                  return <Book book={book} key={book.id} />;
-                })}
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
+    <div className="book">
+      <a href="">
+        <figure className="book__img--wrapper">
+          <img
+           src={book.url}
+            alt=""
+            className="book__img"
+          />
+        </figure>
+      </a>
+      <div className="book__title">
+        <a href="/" className="book__title--link">
+       {book.title}
+        </a>
+      </div>
+      <div className="book__ratings">
+       {
+        new Array(Math.floor(book.rating)).fill(0).map((_, index) => <FontAwesomeIcon icon="star" key={index} />)
+       }
+       {
+        Number.isInteger(book.rating) ? '' : <FontAwesomeIcon icon="star-half-alt" />
+       }
+      </div>
+      <div className="book__price">
+        
+          book.salePrice ? (
+        <> <span className="book__price--normal">${book.originalPrice.toFixed(2)}</span>$
+        ${book.salesPrice.toFixed(2)}
+        </>
+  ) : (
+           <>${ book.originalPrice.toFixed(2) }</>
+       )
+     
+      </div>
     </div>
   );
-};
-
-export default Books;
+}
+export default Book;
