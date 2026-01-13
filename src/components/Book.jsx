b008e4c75641af04a253155c1f0d3b040eb5393f
Your Name <anthony197542@gmail.com>
2026-01-13 12:49:20 -0500

    2com

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,66 +1,36 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Link } from "react-router-dom";
-import Price from "../components/ui/Price";
-import Ratings from "../components/ui/Ratings";
+import { FontAwesomeIcon } from "fontawesome/react-fontawesome";
+import React from "react";
 
-
-const Books = ({ book }) => {
-  const [img, setImg] = useState(null);
-  const mountedRef = useRef(true);
-
-  useEffect(() => {
-    mountedRef.current = true;
-
-    const image = new Image();
-    image.src = book.url;
-
-    image.onload = () => {
-      if (mountedRef.current) {
-        setImg(image);
-      }
-    };
-
-    return () => {
-      mountedRef.current = false;
-    };
-  }, [book.url]);
-
-  return (
-    <div className="book">
-      {img ? (
-        <>
-          <Link to={`/books/${book.id}`}>
-            <figure className="book__img--wrapper">
-              <img
-                src={img.src}
-                alt={book.title}
-                className="book__img"
-              />
-            </figure>
-          </Link>
-
-          <div className="book__title">
-            <Link to={`/books/${book.id}`} className="book__title--link">
-              {book.title}
-            </Link>
-          </div>
-
-          <Ratings rating={book.rating} />
-          <Price
-            salePrice={book.salePrice}
-            originalPrice={book.originalPrice}
-          />
-        </>
-      ) : (
-        <>
-          <div className="book__img--skeleton"></div>
-          <div className="skeleton book__title--skeleton"></div>
-          <div className="skeleton book__rating--skeleton"></div>
-          <div className="skeleton book__price--skeleton"></div>
-        </>
-      )}
-    </div>
-  );
+const Books = () => {
+    return (
+        <div className="book">
+            <a href="">
+                <figure className="book__img--wrapper">
+                    <img
+                    src="https://covers.openlibrary.org/b/id/10523307-L.jpg"
+                    alt=""
+                    className="book__img"
+                    />
+                </figure>
+            </a>
+            <div className="book__title">
+                <a href="/" className="book__title--link">
+                    Atomic  Habits
+                </a>
+            </div>
+            <div className="book__ratings">
+                <FontAwesomeIcon icon="star" />
+                <FontAwesomeIcon icon="star" />
+                <FontAwesomeIcon icon="star" />
+                <FontAwesomeIcon icon="star" />
+                <FontAwesomeIcon icon="star-half-alt" />
+            </div>
+            <div className="book__price">
+                <span className="book__price--normal">$15.00</span> 
+                 $10.00
+        </div>
+        </div>
+    );
 };
 
-export default Books;
+export default Book;
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,64 +1,36 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import Price from "./Price";
-import Rating from "./Rating";
+import { FontAwesomeIcon } from "fontawesome/react-fontawesome";
+import React from "react";
 
-const Book = ({ book }) => {
-    const [img, setImg] = useState();
-
-  const mountedRef = useRef(true);
-
-useEffect(() => {
-    const image = new Image();
-    image.src = book.url;
-    image.onLoad = () => {
-        setTimeout(() => {
-            if (mountedRef.current) {
-           setImg(image);
-            }
-   
-   }, 300);
-    },
-   return () => {
-    // When the component unmounts
-    mountedRef.current = false;
-   }     
-})
-return (
-<div className="book">
-    
-{img ? (
-       <>
-     <Link to={`/books/${book.id}`}>
-<figure className="book__img--wrapper">
-<img 
-src={img.src}
- alt="" 
- className="book__img" 
- 
- />
-</figure>
-</Link>
-<div className="book__title">
-<Link to={`/books/$book.id}` } className="book__title--"link />
-{book.title}
-</Link>
-</div>
-<Rating rating={book.rating} />
-<Price salePrice={book.salePrice} originalPrice={book.original} />
-
-</>
-    ) : (
-        
-    <>
-    <div className="book__img--skeleton"></div>
-    <div className="skeleton book__title--skeleton"></div>
-    <div className="skelton book__rating--skeleton"></div>
-    <div className="skeleton book__price--skeleton"></div>
-</>
-
-</div>
-);
+const Books = () => {
+    return (
+        <div className="book">
+            <a href="">
+                <figure className="book__img--wrapper">
+                    <img
+                    src="https://covers.openlibrary.org/b/id/10523307-L.jpg"
+                    alt=""
+                    className="book__img"
+                    />
+                </figure>
+            </a>
+            <div className="book__title">
+                <a href="/" className="book__title--link">
+                    Atomic  Habits
+                </a>
+            </div>
+            <div className="book__ratings">
+                <FontAwesomeIcon icon="star" />
+                <FontAwesomeIcon icon="star" />
+                <FontAwesomeIcon icon="star" />
+                <FontAwesomeIcon icon="star" />
+                <FontAwesomeIcon icon="star-half-alt" />
+            </div>
+            <div className="book__price">
+                <span className="book__price--normal">$15.00</span> 
+                 $10.00
+        </div>
+        </div>
+    );
 };
+
 export default Book;
